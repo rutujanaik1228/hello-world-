e35890dd501db5bfd12ba31ab57fd2f2637c857e
rutujanaik1228 <83773954+rutujanaik1228@users.noreply.github.com>
2021-05-08 23:50:06 +0530

    Add files via upload

---
+++ python program to find smallest and largest elements of  a list .py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+l=[]
+n=int(input("enter no. of elements:"))
+for i in range(0,n):
+    a=int(input("enter list element :"))
+    l.append(a)
+print("smallest element is :", min(l))
+print("largst element is :",max(l))
+
+
+# In[ ]:
+
+
+
+
