71c7f2454ab9adb839e2366364a86b2a72c501c2
rutujanaik1228 <83773954+rutujanaik1228@users.noreply.github.com>
2021-05-08 23:27:25 +0530

    Add files via upload

---
+++ to find the greatest of three numbers.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n1=int(input("enter 1st no.:"))
+n2=int(input("enter 2nd no.:"))
+n3=int(input("enter 3rd no.:"))
+if n1>n2 and n1>n3:
+    print(n1,"is greatest")  
+elif  n2>n1 and n2>n3:
+    print(n2,"is greatest")  
+else:
+     print(n3,"is greatest")  
+    
+    
+
+
+# In[ ]:
+
+
+
+
