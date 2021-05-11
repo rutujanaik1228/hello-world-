79a68c091656ff434e27364723ac933f55d5ba0b
rutujanaik1228 <83773954+rutujanaik1228@users.noreply.github.com>
2021-05-12 00:00:02 +0530

    Add files via upload

---
+++ python code to print table of a number.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+
+num = int(input("enter a number"))
+print("the table of ",num,":")
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
+
+
+# In[ ]:
+
+
+
+
